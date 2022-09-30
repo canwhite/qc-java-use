522b6896667454c508162a8978170e32d5a53e32
qiaochao <qiaochao@icourt.cc>
2022-09-30 14:28:51 +0800

    feat : steam wrapper

---
+++ q27_stream/Stream.java
@@ -0,0 +1,17 @@
+package q27_stream;
+
+public class Stream {
+
+    public static void main(String[] args) {
+        //list和map获取元素都是get，而array就可以使用中括号操作
+        
+
+
+
+
+        
+    }
+
+
+    
+}
